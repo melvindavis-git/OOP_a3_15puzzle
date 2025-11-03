6afdf82f7b2db882f3cdc88c8303495ecd72fca6
Melvin Davis <melvin.normandavis@gmail.com>
2025-11-03 11:28:39 +0100

    created GUI class

---
+++ src/GUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame implements ActionListener {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
